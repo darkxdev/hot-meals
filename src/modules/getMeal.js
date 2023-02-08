3b1b30043b2dd522199706624d881f74b31642fc
DarkxDev <bryanfernandohurtado@gmail.com>
2023-02-08 11:56:14 -0500

    Async attribute to HTML and base URL for the API

--- src/modules/getMeal.js
+++ src/modules/getMeal.js
@@ -2,9 +2,11 @@
  * Get meal information by name using the MealDB API.
  */
 
+const baseMealByNameURL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+
 const getMealByName = async (mealName) => {
   // Fetch meal information from the API
-  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`);
+  const response = await fetch(`${baseMealByNameURL}${mealName}`);
   const data = await response.json();
 
   // Return the first meal from the API response
